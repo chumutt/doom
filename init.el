f1475137d5bcdd1ff4decba9dc08093bbf20a12d
ChuMutt <chufilthymutt@gmail.com>
2024-06-12 15:30:22 -0500

    config fix

--- init.el
+++ init.el
@@ -22,13 +22,13 @@
 
        :completion
        (company
-         +tng)           ; the ultimate code completion backend
+        +tng)           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;(ivy
-       ;;  +prescient)      ; a search engine for love and life
+       ;; +prescient)      ; a search engine for love and life
        (vertico
-         +icons)           ; the search engine of the future
+        +icons)           ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
@@ -44,15 +44,15 @@
        minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        nav-flash         ; blink cursor line after big motions
-       ;;neotree           ; a project drawer, like NERDTree for vim
+       neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup
-         +defaults)       ; tame sudden yet inevitable temporary windows
+        +defaults)       ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
        treemacs          ; a project drawer, like neotree but cooler
        unicode           ; extended unicode support for various languages
        (vc-gutter
-         +pretty)         ; vcs diff in the fringe
+        +pretty)         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
@@ -60,16 +60,16 @@
 
        :editor
        (evil
-         +everywhere)     ; come to the dark side, we have cookies
+        +everywhere)     ; come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
        format
        ;;god               ; run Emacs commands without modifier keys
-       ;;lispy             ; vim for lisp, for people who don't like vim
+       lispy             ; vim for lisp, for people who don't like vim
        multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
-       ;;parinfer          ; turn lisp into python, sort of
-       ;;rotate-text       ; cycle region at point between text candidates
+       parinfer          ; turn lisp into python, sort of
+       rotate-text       ; cycle region at point between text candidates
        snippets          ; my elves. They type so I don't have to
        word-wrap         ; soft wrapping with language-aware indent
 
@@ -88,17 +88,17 @@
 
        :checkers
        (syntax
-         +flyspell)              ; tasing you for every semicolon you forget
+        +flyspell)              ; tasing you for every semicolon you forget
        (spell
-         +flyspell)         ; tasing you for misspelling mispelling
-       ;;grammar           ; tasing grammar mistake every you make
+        +flyspell)         ; tasing you for misspelling mispelling
+       grammar           ; tasing grammar mistake every you make
 
        :tools
-       ;;ansible
+       ansible
        biblio            ; Writes a PhD for you (citation needed)
        ;;debugger          ; FIXME stepping through code, to help you add bugs
-       ;;direnv
-       ;;docker
+       direnv
+       docker
        editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval
@@ -113,7 +113,7 @@
        rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
-       ;;tmux              ; an API for interacting with tmux
+       tmux              ; an API for interacting with tmux
        tree-sitter       ; syntax and parsing, sitting in a tree...
        upload            ; map local to remote projects via ssh/ftp
 
@@ -125,16 +125,16 @@
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
        (cc
-         +lsp
-         +tree-sitter)            ; C > C++ == 1
-       ;;clojure           ; java with a lisp
+        +lsp
+        +tree-sitter)            ; C > C++ == 1
+       clojure           ; java with a lisp
        common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
-       ;;csharp            ; unity, .NET, and mono shenanigans
+       csharp            ; unity, .NET, and mono shenanigans
        data              ; config/data formats
-       ;;(dart
-       ;; +flutter)        ; paint ui and not much else
+       (dart
+        +flutter)        ; paint ui and not much else
        ;;dhall
        ;;elixir            ; erlang done right
        ;;elm               ; care for a cup of TEA?
@@ -148,52 +148,52 @@
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
        (go
-         +lsp)            ; the hipster dialect
+        +lsp)            ; the hipster dialect
        (graphql +lsp)    ; Give queries a REST
        (haskell
-         +lsp)            ; a language that's lazier than I am
+        +lsp)            ; a language that's lazier than I am
        hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
        json              ; At least it ain't XML
-       ;;(java
-       ;; +lsp            ; the poster child for carpal tunnel syndrome
-       ;;(javascript
-       ;; +lsp
-       ;; +tree-sitter)    ; all(hope(abandon(ye(who(enter(here))))))
+       (java
+        +lsp)            ; the poster child for carpal tunnel syndrome
+       (javascript
+        +lsp
+        +tree-sitter)    ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        latex             ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ledger            ; be audit you can be
-       ;;lua               ; one-based indices? one-based indices
+       lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org
-         +roam2
-         +hugo
-         +pandoc
-         +journal
-         +noter
-         +gnuplot
-         +dragndrop)      ; organize your plain life in plain text
+        +roam2
+        +hugo
+        +pandoc
+        +journal
+        +noter
+        +gnuplot
+        +dragndrop)      ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        plantuml          ; diagrams for confusing people more
-       ;;purescript        ; javascript, but functional
-       ;;python            ; beautiful is better than ugly
-       ;;qt                ; the 'cutest' gui framework ever
+       purescript        ; javascript, but functional
+       python            ; beautiful is better than ugly
+       qt                ; the 'cutest' gui framework ever
        racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
-       ;;(ruby
-       ;; +rails)          ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;;(rust
-       ;; +lsp)            ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       (ruby
+        +rails)          ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       (rust
+        +lsp)            ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
-       ;; (scheme
-       ;; +guile)          ; a fully conniving family of lisps
+       (scheme
+        +guile)          ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
