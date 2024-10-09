bf09951e8ef766eafd87283950cbaa596727a41d
ChuMutt <chufilthymutt@gmail.com>
2024-10-09 18:48:58 -0500

    tangle

--- config.el
+++ config.el
@@ -348,30 +348,30 @@
 (after! 'org-archive
   (setq org-archive-default-command 'org-archive-subtree-hierarchical))
 
-(setq org-capture-templates
-  '(("t" "Personal todo" entry
-    (file+headline +org-capture-todo-file "Inbox")
-  "* [ ] %?\n%i\n%a" :prepend t)
-  ("n" "Personal notes" entry
-  (file+headline +org-capture-notes-file "Inbox")
-  "* %u %?\n%i\n%a" :prepend t)
-  ("j" "Journal" entry
-  (file+olp+datetree +org-capture-journal-file)
-  "* %U %?\n%i\n%a" :prepend t)
-  ("p" "Templates for projects")
-  ("pt" "Project-local todo" entry
-  (file+headline +org-capture-project-todo-file "Inbox")
-  "* TODO %?\n%i\n%a" :prepend t)
-  ("pn" "Project-local notes" entry
-  (file+headline +org-capture-project-notes-file "Inbox")
-  "* %U %?\n%i\n%a" :prepend t)
-  ("pc" "Project-local changelog" entry
-  (file+headline +org-capture-project-changelog-file "Unreleased")
-  "* %U %?\n%i\n%a" :prepend t)
-  ("o" "Centralized templates for projects")
-  ("ot" "Project todo" entry #'+org-capture-central-project-todo-file "* TODO %?\n %i\n %a" :heading "Tasks" :prepend nil)
-  ("on" "Project notes" entry #'+org-capture-central-project-notes-file "* %U %?\n %i\n %a" :heading "Notes" :prepend t)
-  ("oc" "Project changelog" entry #'+org-capture-central-project-changelog-file "* %U %?\n %i\n %a" :heading "Changelog" :prepend t)))
+  (setq org-capture-templates
+    '(("t" "Personal todo" entry
+      (file+headline +org-capture-todo-file "Inbox")
+    "* [ ] %?\n%i\n%a" :prepend t)
+    ("n" "Personal notes" entry
+    (file+headline +org-capture-notes-file "Inbox")
+    "* %u %?\n%i\n%a" :prepend t)
+    ("j" "Journal" entry
+    (file+olp+datetree +org-capture-journal-file)
+    "* %U %?\n%i\n%a" :prepend t)
+    ("p" "Templates for projects")
+    ("pt" "Project-local todo" entry
+    (file+headline +org-capture-project-todo-file "Inbox")
+    "* TODO %?\n%i\n%a" :prepend t)
+    ("pn" "Project-local notes" entry
+    (file+headline +org-capture-project-notes-file "Inbox")
+    "* %U %?\n%i\n%a" :prepend t)
+    ("pc" "Project-local changelog" entry
+    (file+headline +org-capture-project-changelog-file "Unreleased")
+    "* %U %?\n%i\n%a" :prepend t)
+    ("o" "Centralized templates for projects")
+    ("ot" "Project todo" entry #'+org-capture-central-project-todo-file "* TODO %?\n %i\n %a" :heading "Tasks" :prepend nil)
+    ("on" "Project notes" entry #'+org-capture-central-project-notes-file "* %U %?\n %i\n %a" :heading "Notes" :prepend t)
+    ("oc" "Project changelog" entry #'+org-capture-central-project-changelog-file "* %U %?\n %i\n %a" :heading "Changelog" :prepend t)))
 
 (after! 'org
   (use-package! vulpea
@@ -516,6 +516,8 @@
 
 (after! sly
   (setq sly-complete-symbol-function 'sly-flex-completions))
+
+(setq c-syntactic-indentation nil)
 
 (use-package mu4e
   ;; :ensure nil
