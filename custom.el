61cd22e24bad97f3cb6b1265f0eb46dfa1475dc4
ChuMutt <chufilthymutt@gmail.com>
2024-10-23 12:20:01 -0500

    track

---
+++ custom.el
@@ -0,0 +1,14 @@
+;;; custom.el -*- lexical-binding: t; -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("/home/chu/nextcloud/documents/org/roam/math/20240903162832-linear_algebra.org" "/home/chu/nextcloud/documents/org/roam/c++/20240116111203-cpp.org" "/home/chu/nextcloud/documents/org/roam/20220726210346-important_dates.org" "/home/chu/nextcloud/documents/org/roam/20221002190906-furry.org" "/home/chu/nextcloud/documents/org/roam/20221004221829-todo.org" "/home/chu/nextcloud/documents/org/roam/20240201170253-albums_to_download.org" "/home/chu/nextcloud/documents/org/roam/20240326161621-livestreaming.org" "/home/chu/nextcloud/documents/org/roam/asm/20240830094040-assembly.org" "/home/chu/nextcloud/documents/org/roam/daily/2024-05-08.org" "/home/chu/nextcloud/documents/org/roam/engl/engl-2020/20240116095712-engl_2020.org" "/home/chu/nextcloud/documents/org/roam/hist/hist-2320/20240116133242-hist_2320.org" "/home/chu/nextcloud/documents/org/roam/lisp/scheme/sicp/README.org" "/home/chu/nextcloud/documents/org/roam/math/20220821114043-mathematics.org" "/home/chu/nextcloud/documents/org/roam/math/20240905211621-calculus_ii.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
