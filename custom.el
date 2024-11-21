d73e2596686181df92491c626c2ba426a4b95d47
ChuMutt <chufilthymutt@gmail.com>
2024-11-20 18:15:51 -0600

    updates

--- custom.el
+++ custom.el
@@ -4,8 +4,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ignored-local-variable-values '((eval setq org-export-initial-scope 'subtree)))
  '(org-agenda-files
-   '("/home/chu/nextcloud/documents/org/roam/math/20240903162832-linear_algebra.org" "/home/chu/nextcloud/documents/org/roam/c++/20240116111203-cpp.org" "/home/chu/nextcloud/documents/org/roam/20220726210346-important_dates.org" "/home/chu/nextcloud/documents/org/roam/20221002190906-furry.org" "/home/chu/nextcloud/documents/org/roam/20221004221829-todo.org" "/home/chu/nextcloud/documents/org/roam/20240201170253-albums_to_download.org" "/home/chu/nextcloud/documents/org/roam/20240326161621-livestreaming.org" "/home/chu/nextcloud/documents/org/roam/asm/20240830094040-assembly.org" "/home/chu/nextcloud/documents/org/roam/daily/2024-05-08.org" "/home/chu/nextcloud/documents/org/roam/engl/engl-2020/20240116095712-engl_2020.org" "/home/chu/nextcloud/documents/org/roam/hist/hist-2320/20240116133242-hist_2320.org" "/home/chu/nextcloud/documents/org/roam/lisp/scheme/sicp/README.org" "/home/chu/nextcloud/documents/org/roam/math/20220821114043-mathematics.org" "/home/chu/nextcloud/documents/org/roam/math/20240905211621-calculus_ii.org")))
+   '("/home/chu/nextcloud/Documents/org/roam/c++/20240116111203-cpp.org" "/home/chu/nextcloud/Documents/org/roam/20221002161620-my_conlang.org" "/home/chu/nextcloud/Documents/org/roam/20221002190638-conlangs.org" "/home/chu/nextcloud/Documents/org/roam/20221004222235-notes.org" "/home/chu/nextcloud/Documents/org/roam/20220726210346-important_dates.org" "/home/chu/nextcloud/Documents/org/roam/math/calc-ii/20240905211621-calculus_ii.org" "/home/chu/nextcloud/Documents/org/roam/math/linear-algebra/20240903162832-linear_algebra.org" "/home/chu/nextcloud/Documents/org/roam/20221004221829-todo.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
