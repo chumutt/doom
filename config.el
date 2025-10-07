84029c458e403648bd187caf660e52b0f78b4d10
chumutt <chufilthymutt@gmail.com>
2025-10-06 21:26:41 -0500

    tangle

--- config.el
+++ config.el
@@ -391,6 +391,23 @@
 
         ))
 
+(setq org-capture-templates
+  (append org-capture-templates
+    '(
+      ("t" "Personal todo" entry (file+headline +org-capture-todo-file "Inbox") "* [ ] %?\n%i\n" :prepend t)
+      ("n" "Personal notes" entry (file+headline +org-capture-notes-file "Inbox") "* %u %?\n%i\n" :prepend t)
+      ("j" "Journal" entry (file+olp+datetree +org-capture-journal-file) "* %U %?\n%i\n" :prepend t)
+      ("p" "Templates for projects")
+      ("pt" "Project-local todo" entry (file+headline +org-capture-project-todo-file "Inbox") "* TODO %?\n%i\n%a" :prepend t)
+      ("pn" "Project-local notes" entry (file+headline +org-capture-project-notes-file "Inbox") "* %U %?\n%i\n%a" :prepend t)
+      ("pc" "Project-local changelog" entry (file+headline +org-capture-project-changelog-file "Unreleased") "* %U %?\n%i\n%a" :prepend t)
+      ("o" "Centralized templates for projects")
+      ("ot" "Project todo" entry #'+org-capture-central-project-todo-file "* TODO %?\n%i\n%a" :heading "Tasks" :prepend nil)
+      ("on" "Project notes" entry #'+org-capture-central-project-notes-file "* %U %?\n%i\n%a" :heading "Notes" :prepend t)
+      ("oc" "Project changelog" entry #'+org-capture-central-project-changelog-file "* %U %?\n%i\n%a" :heading "Changelog" :prepend t)
+      ("b" "Bookmarks" entry (file+headline +org-capture-bookmarks-file "Inbox") "* %^{Title}\n%U\n%i\n%a" :prepend t)
+      )))
+
 (after! org
   (add-to-list 'org-modules 'org-habit)
   (setq org-habit-graph-column 60))
@@ -628,14 +645,3 @@
     )
   )
 (advice-add 'org-download-clipboard :before #'win2wsl-clipped-image)
-
-(after! org
-  (setq org-agenda-custom-commands
-        '(("u"                     ; key after `C-c a`
-           "Untagged TODO items"   ; buffer title
-           tags-todo               ; only TODO headlines
-           "+TODO=\"TODO\"+TAGS=\"\"" ; TODO state + no tags
-           ((org-agenda-overriding-header "Untagged TODOs")
-            ;; skip items whose TODO keyword is a DONE state
-            (org-agenda-skip-function
-             '(org-agenda-skip-entry-if 'todo 'done)))))))
